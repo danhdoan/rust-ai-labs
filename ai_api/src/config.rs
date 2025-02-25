eefc2d105ee642db3349b6a8207e522dfd1857b2
Danh Doan <danhdoancv@gmail.com>
2025-02-25 18:12:06 +0700

    DONE #9 - perform inference with GPU

--- ai_api/src/config.rs
+++ ai_api/src/config.rs
@@ -1,2 +1,2 @@
-pub const ONNX_MODEL_PATH: &str = "./models/sample-resnet18.onnx";
-pub const IMAGE_CLASS_PATH: &str = "./models/classes.txt";
+pub const PYTORCH_MODEL_PATH: &str = "./models/resnet18_torchscript.pt";
+pub const IMAGE_CLASS_PATH: &str = "./models/imagenet_classes.txt";
