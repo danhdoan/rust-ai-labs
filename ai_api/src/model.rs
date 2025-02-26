3271f9a93ee242140762fbe90414ee472e944b4e
Danh Doan <danhdoancv@gmail.com>
2025-02-26 11:52:27 +0700

    DONE #14 - refactor inference code

--- ai_api/src/model.rs
+++ ai_api/src/model.rs
@@ -1,7 +1,7 @@
 use axum::{http::StatusCode, Json};
 use lazy_static::lazy_static;
 use std::sync::Arc;
-use tch::{CModule, Device};
+use tch::{CModule, Device, Tensor};
 
 use crate::config::{IMAGE_CLASS_PATH, PYTORCH_MODEL_PATH};
 use crate::errors::{handle_error, ErrorCode, ErrorResponse};
@@ -19,27 +19,32 @@ lazy_static! {
         load_classes(IMAGE_CLASS_PATH).expect("Failed to load class file");
 }
 
-pub fn run_classification(
-    image_bytes: Vec<u8>,
-) -> Result<String, (StatusCode, Json<ErrorResponse>)> {
-    let tensor = preprocess_image(image_bytes)
-        .map_err(|err| handle_error(ErrorCode::InvalidInputData, err))?;
-
+pub fn perform_inference(tensor: Tensor) -> Result<String, (ErrorCode, String)> {
     let device = Device::Cuda(0);
     let tensor = tensor.to_device(device);
     let _guard = tch::no_grad_guard();
 
     let output = MODEL
         .forward_ts(&[tensor])
-        .map_err(|err| handle_error(ErrorCode::InferenceFailed, err))?;
+        .map_err(|err| (ErrorCode::InferenceFailed, err.to_string()))?;
 
     let probs = output.softmax(-1, tch::Kind::Float);
     let max_idx = probs.argmax(-1, false).int64_value(&[]);
     if (max_idx as usize) < CLASSES.len() {
         return Ok(CLASSES[max_idx as usize].clone());
     }
-    Err(handle_error(
+    Err((
         ErrorCode::OutputConversionFailed,
-        "Class Index Out of Bound",
+        "Class Index Out of Bound".to_string(),
     ))
+}
+pub fn run_classification(
+    image_bytes: Vec<u8>,
+) -> Result<String, (StatusCode, Json<ErrorResponse>)> {
+    let tensor = preprocess_image(image_bytes)
+        .map_err(|err| handle_error(ErrorCode::InvalidInputData, err))?;
+
+    let output =
+        perform_inference(tensor).map_err(|(err_code, err_msg)| handle_error(err_code, err_msg))?;
+    Ok(output)
 }
